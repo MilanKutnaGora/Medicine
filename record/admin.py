2e0f600f3eaa3919b3e115f0eca4dbf9b1d0ff8c
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-03-27 05:46:29 +0300

    Step 08 finish

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from record.models import Record
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    ordering = ('id',)
+
+
+
+
+
--- record/models.py
+++ record/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class Record(models.Model):
     name = models.CharField(max_length=100, verbose_name="заголовок")
-    slug = models.CharField(max_length=100, null=True, blank=True, verbose_name="слаг")
     description = models.TextField(null=False, verbose_name="содержимое")
-    image = models.ImageField(upload_to='media/',  verbose_name='изображение')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
     publication = models.BooleanField(default=True, verbose_name="признак публикации")
     views_count = models.IntegerField(default=0, verbose_name="колличество просмотров")
